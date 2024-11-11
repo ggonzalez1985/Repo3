db878165e89f30002b2571e174d05085a1253445
ggonzalez1985 <gabrielgonzalez1985@gmail.com>
2024-11-10 22:03:33 -0300

    Ahora si!

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58279ca5865927f34b4eb04817a3ca6cbd28cebf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7662799824a38c3693f86a179a079b77b2c3262")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
